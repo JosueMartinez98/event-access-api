69beed1ed0a2c2e5d34e0fd2a9353c481ce63e32
Josue <josueortegamtz98@gmail.com>
2022-03-26 16:17:39 -0600

    first-phase|feat: add basic implementation of swagger and include empty .env and .yml files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Events Access API')
+    .setDescription('The API to manage the data about the UI of Events Access Application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
