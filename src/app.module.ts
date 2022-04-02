bbc14f655463a32795bba19b3ad069882c228a13
Josue <josueortegamtz98@gmail.com>
2022-04-02 09:28:22 -0600

    first-phase|feat: add .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number.parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const apiPrefix = process.env.API_PREFIX;
   const config = new DocumentBuilder()
     .setTitle('Events Access API')
     .setDescription('The API to manage the data about the UI of Events Access Application')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('v1/docs', app, document);
-
-  await app.listen(3000);
+  SwaggerModule.setup(`${apiPrefix}/docs`, app, document);
+  app.setGlobalPrefix(apiPrefix);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
